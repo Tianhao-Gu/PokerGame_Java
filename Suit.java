db42be30c65ff79377eea1a791c473c9cdf8d084
TitiMeow <titigu1987@gmail.com>
2013-06-01 00:07:15 -0700

    Poker Game - Suit Class

---
+++ Suit.java
@@ -0,0 +1,29 @@
+
+/**
+ * This class defines Suit interface
+ * @author Titi Gu
+ *
+ */
+
+
+class Suit 
+{
+	static final int CLUBS = 1;
+	static final int DIAMONDS = 2;
+	static final int HEARTS = 3;
+	static final int SPADES = 4;   
+	int suitValue;    
+	Suit(int i) { suitValue = i; }
+	public String toString() 
+	{
+		switch (suitValue) 
+		{      
+			case CLUBS: return "clubs";       
+			case DIAMONDS: return "diamonds";
+			case HEARTS: return "hearts";
+			case SPADES: return "spades";
+			default: return "error";
+		}
+	}
+}
+
